1bc8c1164ad66e3f827e8be677e465c1dfba4813
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2023-10-18 01:24:37 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+//jshint esversion:6
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import ejs from "ejs";
+import mongoose from "mongoose";
+
+const app = express();
+const port = 3000;
+
+const uri = "mongodb+srv://danieldeveloper:g16laqyjwRIUEdZC@clusterforuser.fdrcpjr.mongodb.net/?retryWrites=true&w=majority";
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// mongoose.connect(uri,  {useNewUrlParser: true}).then(() => console.log('Connected!'))
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const UserDB = new mongoose.model("User", userSchema)
+
+// app.use(async (req, res, next) => {
+//     try {
+//      await mongoose.connect(uri,  {useNewUrlParser: true}).then(() => console.log('Connected!'))
+//     } catch (error) {
+//         res.send(err.message)
+//     }
+//     next() });
+
+app.get("/", async (req, res) => {
+    try {
+        res.render('home')
+    } catch (error) {
+        res.send(error.message)
+    }
+});
+
+
+app.get("/login", async (req, res) => {
+    try {
+        res.render('login')
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+
+app.get("/register", async (req, res) => {
+    try {
+        res.render('register')
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+
+
+app.post("/register", async (req, res) => {
+
+    try {
+
+
+        const email = req.body.username
+        const password = req.body.password
+
+
+        const newUser = new UserDB({
+            email: req.body.username,
+            password: req.body.password
+        });
+
+        newUser.save(function(err){
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        })
+
+
+        res.render('home')
+    } catch (error) {
+        res.send(error.message)
+    }
+
+    
+})
+    
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
