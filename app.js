0084e52bd18d067e1c5666939ae97ae1d2473187
Daniel Mendes de Albuquerque <daniel_developer@hotmail.com>
2023-10-18 21:27:24 -0300

    login statement done

--- app.js
+++ app.js
@@ -4,114 +4,107 @@ import bodyParser from "body-parser";
 import axios from "axios";
 import ejs from "ejs";
 import mongoose from "mongoose";
-import 'dotenv/config';
-
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 3001;
-const uri = process.env.DB_CONN
+const uri = process.env.DB_CONN;
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(uri,  {useNewUrlParser: true}).then(() => console.log('Connected!'))
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => console.log("Connected!"));
 
 const userSchema = {
-    email: String,
-    password: String
-}
+  email: String,
+  password: String,
+};
 
-const UserDB = new mongoose.model("User", userSchema)
-
-// app.use(async (req, res, next) => {
-//     try {
-//      await mongoose.connect(uri,  {useNewUrlParser: true}).then(() => console.log('Connected!'))
-//     } catch (error) {
-//         res.send(err.message)
-//     }
-//     next() });
+const userDB = new mongoose.model("User", userSchema);
 
 app.get("/", async (req, res) => {
-    try {
-        res.render('home')
-    } catch (error) {
-        res.send(error.message)
-    }
+  try {
+    res.render("home");
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 
 app.get("/login", async (req, res) => {
-    try {
-        res.render('login')
-    } catch (error) {
-        res.send(error.message)
-    }
-})
+  try {
+    res.render("login");
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
 
 app.get("/register", async (req, res) => {
-    try {
-        res.render('register')
-    } catch (error) {
-        res.send(error.message)
-    }
-})
+  try {
+    res.render("register");
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
 
 app.post("/register", async (req, res) => {
+  // const email = req.body.username
+  // const password = req.body.password
 
-        // const email = req.body.username
-        // const password = req.body.password
+  //Data from form in register.ejs and inserted to the MongoDB schema(email, password);
+  const newUser = new userDB({
+    email: req.body.username,
+    password: req.body.password,
+  });
 
-        const newUser = new UserDB({
-            email: req.body.username,
-            password: req.body.password
-        });
+  //MongoDB save() method updates an existing document or inserts a document depending on the parameter.
+  newUser
+    .save()
+    .then(res.status(201).render("secrets"))
 
-        newUser.save().then(res.status(201).render("secrets")) 
-        .catch((err) => {
-            res.status(500).send({message: `${err.message} - falha ao cadastrar.`})
-        })
-    })
-
-// app.post("/login", function(req, res){
-
-//     const userNameLogin = req.body.email;
-//     const userPasswordLogin = req.body.password;
-//     UserDB.findOne({email: userNameLogin}, checkFunction)
-
-//     function checkFunction(error, foundUser){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             if ()
-//         }
-//     }
-
-// })
-        // function(err){
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         res.render("secrets");
-        //     }
-        // })
-
-    // } catch (error) {
-    //     res.send("catchError" + error.message)
-    // }
-// })
+    .catch((err) => {
+      res.status(500).send({ message: `${err.message} - falha ao cadastrar.` });
+    });
+});
 
 
+app.post("/login", async (req, res) => {
+    //Mongoose dropped callback now you need to use try/catch
 
+  try {
 
+    //Getting Data from form input in login.ejs
+    const userNameLogin = req.body.username
+    const userPasswordLogin = req.body.password
 
+    //MongoDB findOne() method returns only one document that satisfies the criteria entered when no document is found it returns a null.
+    const dbData = await userDB.findOne({email: userNameLogin})
+    console.log(dbData)
 
+    if (dbData) {
 
+    //comparing Data in database with form input data in login.ejs
+      if (dbData.password === userPasswordLogin) {
+        res.render("secrets");
+      } 
 
+      else {
+        res.send("ErrorPassword");
+        console.log("Error");
+      }
+
+    }}
+    catch (err) {
+    res.send("No data found " + err);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-  
+  console.log(`Server is running on port ${port}`);
+});
